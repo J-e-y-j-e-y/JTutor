72f401f5f44e167c1512f3e1f2b3ab90ab78eb21
alexey.korotkov <a.u.korotkov@gmail.com>
2025-01-29 11:29:47 +0300

    solved problem

---
+++ src/main/java/homework/oop/classes_homework/task_3/Duck.java
@@ -0,0 +1,13 @@
+package homework.oop.classes_homework.task_3;
+
+public class Duck extends Animal {
+    String beakColor = "Yellow";
+
+    public void swim() {
+        System.out.println("Duck is swimming");
+    }
+
+    public void quack() {
+        System.out.println("Duck is quacking");
+    }
+}
