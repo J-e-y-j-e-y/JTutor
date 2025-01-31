938c9cbb842b8c42e3d6316e07b615d9c040ebc0
Alexey Korotkov <a.u.korotkov@gmail.com>
2025-01-31 20:46:13 +0300

    uncomment class code

--- src/main/java/homework/oop/interface_homework/task_2/NumberArrayFilter.java
+++ src/main/java/homework/oop/interface_homework/task_2/NumberArrayFilter.java
@@ -2,16 +2,16 @@ package homework.oop.interface_homework.task_2;
 
 import java.util.ArrayList;
 
-//public class NumberArrayFilter implements Filter{
-//    @Override
-//    public boolean apply(Object o) {
-//        for (Object object : o) { // не понимаю что не нравится и как починить
-//            for (char c : object) { // не понимаю что не нравится и как починить
-//                if (Character.isDigit(c)) {
-//                    return false;
-//                }
-//            }
-//        }
-//        return true;
-//    }
-//}
+public class NumberArrayFilter implements Filter {
+    @Override
+    public boolean apply(Object o) {
+        for (Object object : o) { // не понимаю что не нравится и как починить
+            for (char c : object) { // не понимаю что не нравится и как починить
+                if (Character.isDigit(c)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
