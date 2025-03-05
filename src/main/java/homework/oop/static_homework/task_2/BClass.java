2671824bc589aab1f976b510a9bff5132c4cb7d9
DenisMikhno_home <denismikhno@gmail.com>
2025-03-05 20:11:49 +0300

    Выполнил static_task_2

--- src/main/java/homework/oop/static_homework/task_2/BClass.java
+++ src/main/java/homework/oop/static_homework/task_2/BClass.java
@@ -3,11 +3,16 @@ package homework.oop.static_homework.task_2;
 public class BClass {
     static int bCounter;
 
+    public BClass() {
+        System.out.println("В конструкторе B класса bCounter = " + bCounter);
+    }
+
     static {
-        bCounter++;
+        //bCounter++;
     }
 
     {
-        bCounter = 0;
+        bCounter ++;
+        System.out.println("Увеличил счетчик. bCounter = " + bCounter);
     }
 }
