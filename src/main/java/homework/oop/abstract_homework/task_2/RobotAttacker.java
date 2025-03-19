173724d6d5a013cac15a967c2557acc0f63ef39d
denismikhno_home <denismikhno@gmail.com>
2025-03-19 19:56:23 +0300

    Выполнил abstract_homework.task_2

---
+++ src/main/java/homework/oop/abstract_homework/task_2/RobotAttacker.java
@@ -0,0 +1,10 @@
+package homework.oop.abstract_homework.task_2;
+
+public class RobotAttacker extends AbstractRobot implements Attackable {
+
+    public RobotAttacker(String name, String team) {
+        super(name, team);
+    }
+
+
+}
