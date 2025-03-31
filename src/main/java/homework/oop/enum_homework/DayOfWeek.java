d685aa0ca06841ad1eaa8283810ec3bc39a8bb0f
denismikhno_home <denismikhno@gmail.com>
2025-03-31 18:18:21 +0300

    Добавил в задание abstract_homework.task_1 класс Bouquet и геттер для поля pricePerPiece.

--- src/main/java/homework/oop/enum_homework/DayOfWeek.java
+++ src/main/java/homework/oop/enum_homework/DayOfWeek.java
@@ -1,5 +1,22 @@
 package homework.oop.enum_homework;
 
 public enum DayOfWeek {
-    MONDAY
+    MONDAY("Понедельник"),
+    TUESDAY("Вторник"),
+    Wednesday("Среда"),
+    Thursday("Четверг"),
+    Friday("Пятница"),
+    Saturday("Суббота"),
+    Sunday("Воскресенье");
+
+    String day;
+
+    DayOfWeek(String day) {
+        this.day = day;
+    }
+
+    @Override
+    public String toString() {
+        return day;
+    }
 }
