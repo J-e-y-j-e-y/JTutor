bf3c4a04bd3ddfa38e5379af2c3df58d8032a39a
denismikhno_home <denismikhno@gmail.com>
2025-04-02 19:16:52 +0300

    Все исправил в static_task_1!!!

--- src/main/java/homework/oop/enum_homework/DayOfWeek.java
+++ src/main/java/homework/oop/enum_homework/DayOfWeek.java
@@ -1,22 +1,5 @@
 package homework.oop.enum_homework;
 
 public enum DayOfWeek {
-    MONDAY("Понедельник"),
-    TUESDAY("Вторник"),
-    Wednesday("Среда"),
-    Thursday("Четверг"),
-    Friday("Пятница"),
-    Saturday("Суббота"),
-    Sunday("Воскресенье");
-
-    String day;
-
-    DayOfWeek(String day) {
-        this.day = day;
-    }
-
-    @Override
-    public String toString() {
-        return day;
-    }
+    MONDAY
 }
--- src/main/java/homework/oop/enum_homework/Task.java
+++
@@ -1,4 +0,0 @@
-package homework.oop.enum_homework;
-
-public class Task {
-}
