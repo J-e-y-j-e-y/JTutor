292ab73cd065aa6c998480e99de9a5d32b36e45c
denismikhno_laptop <denismikhno@gmail.com>
2025-04-18 12:35:52 +0300

    Выполнил nested_classes.local.task_1

---
+++ src/main/java/homework/oop/nested_classes_homework/local_classes_homework/task_1/Main.java
@@ -0,0 +1,9 @@
+package homework.oop.nested_classes_homework.local_classes_homework.task_1;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("log1", "pass1");
+        user.createQuery();
+
+    }
+}
