4144078e9c828ad1e3bd3fe0e8f5dc4053ca7143
Jey <Nightmare-000@yandex.ru>
2025-04-25 13:49:22 +0300

    collection lessons examples: for each, iterator, comparator, list

---
+++ src/main/java/lessons/collections/comparator/ex2/Player.java
@@ -0,0 +1,70 @@
+package lessons.collections.comparator.ex2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Player {
+    private int ranking;
+    private String name;
+    private int age;
+
+    public Player(int ranking, String name, int age) {
+        this.ranking = ranking;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        List<Player> footballTeam = new ArrayList<>();
+        Player player1 = new Player(59, "John", 20);
+        Player player2 = new Player(67, "Roger", 22);
+        Player player3 = new Player(45, "Steven", 24);
+        footballTeam.add(player1);
+        footballTeam.add(player2);
+        footballTeam.add(player3);
+
+        System.out.println("Before Sorting : " + footballTeam);
+        PlayerRankingComparator playerRankComparator = new PlayerRankingComparator();
+        Collections.sort(footballTeam, playerRankComparator);
+        System.out.println("After Sorting : " + footballTeam);
+        System.out.println();
+        System.out.println("Before Sorting : " + footballTeam);
+        PlayerAgeComparator playerAgeComparator = new PlayerAgeComparator();
+        Collections.sort(footballTeam, playerAgeComparator);
+        System.out.println("After Sorting : " + footballTeam);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "ranking=" + ranking +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
